8216e00588a5e5818711dd4f56c094718ae32d5d
joseluisgarcia-1 <jlgarcia002@misena.edu.co>
2021-11-18 20:08:45 -0500

    try_except

---
+++ try_except.py
@@ -0,0 +1,17 @@
+print("****función que recibe un número como parámetro y retornar una lista con todos los divisores número")
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+def run():
+    try:
+        num = int(input("Ingresa un número: "))
+        print(divisors(num))
+        print("Terminó mi programa")
+    except ValueError:
+        print("Debes ingresar un número")
+if __name__ == '__main__':
+    run()
